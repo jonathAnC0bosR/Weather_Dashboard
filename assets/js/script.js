a540085611228d0ec5ff546758b8c23a09d59459
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-04-18 12:55:12 -0600

    Added mock json file to not waste api calls in the main webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
 var apiKey = 'e02041905f5a14c5c120f521c211aa9b';
-console.log(apiKey);
+var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=' + apiKey;
+var inputEl = document.querySelector("#input-city");
+var searchBtn = document.querySelector(".btn-el");
+var city;
+
+searchBtn.addEventListener('click', function(event) {
+    event.preventDefault;
+    city = inputEl.value;
+    
+})
+
+// fetch(apiURL);
+
+var isTrue = false;
+fetch(isTrue ? 'endpoint' : './mock.json')
+    .then 
