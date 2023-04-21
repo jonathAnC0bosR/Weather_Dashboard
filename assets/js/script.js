7210c25e01d4b31f12deda1f852c04209f18fcc5
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-04-21 01:02:16 -0600

    Added CSS and HTML elements to the document

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var apiKey = 'e02041905f5a14c5c120f521c211aa9b';
 //DOM ELEMENTS
 var inputEl = document.querySelector('#input-city');
 var searchBtn = document.querySelector('.btn-el');
+var bigCardEl = document.querySelector('.big-card')
 
 var handleFormSubmit = function(event) {
     event.preventDefault();
@@ -23,7 +24,7 @@ var getCityCoordenates = function(city) {
             if(response.ok) {
                 response.json().then(function(data) {
                     console.log(data);
-                    displayWeatherCards(data.list)
+                    displayWeatherCards(data)
                 })
             } else { 
                 alert('Error' + response.statusText);
@@ -34,7 +35,17 @@ var getCityCoordenates = function(city) {
 }
 
 var displayWeatherCards = function(data) {
-    
+    if(data === 0) {
+        alert("There's no data for this city, sorry");
+        return;
+    } 
+
+    bigCardEl.textContent = inputEl.value.trim();
+
+    for(var i = 0; i < data.length; i++) {
+        console.log(data[i].list);
+    }
+
 }
 
 searchBtn.addEventListener('click', handleFormSubmit);
