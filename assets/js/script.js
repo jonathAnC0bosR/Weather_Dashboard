57f956c65b797ac4bdfde4a7085e0c03a6f40c03
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-04-20 22:56:55 -0600

    Added function that retrieves data from last 5 days

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,40 @@
 var apiKey = 'e02041905f5a14c5c120f521c211aa9b';
-var queryURL;
 
 //DOM ELEMENTS
 var inputEl = document.querySelector('#input-city');
 var searchBtn = document.querySelector('.btn-el');
 
-var getCityCoordenates = function(city) {
+var handleFormSubmit = function(event) {
+    event.preventDefault();
     var city = inputEl.value.trim();
-    queryURL =  "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-    fetch(queryURL)
-    .then.
 
+    if (city) {
+         getCityCoordenates(city);
+        console.log(city);
+    } else {
+        alert('Please enter a city');
+    }
 }
 
-var getCityData = function(lat, lon) {
-
+var getCityCoordenates = function(city) {
+    var queryUrl =  'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey + '&units=imperial';
+    fetch(queryUrl)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                    displayWeatherCards(data.list)
+                })
+            } else { 
+                alert('Error' + response.statusText);
+            }
+        }).catch(function(error){
+            alert('Unable to connect to Weather Server');
+        })
 }
 
-searchBtn.addEventListener('click', getCityCoordenates);
+var displayWeatherCards = function(data) {
+    
+}
+
+searchBtn.addEventListener('click', handleFormSubmit);
