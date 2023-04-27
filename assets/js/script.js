c70b63921b687adc7be6a4faf4d8bdec1ec7ff94
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-04-26 23:35:47 -0600

    Added the code that dinamically adds icons depending on the current weather and added the code that fetches the current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ var apiKey = 'e02041905f5a14c5c120f521c211aa9b';
 //DOM ELEMENTS
 var inputEl = document.querySelector('#input-city');
 var searchBtn = document.querySelector('.btn-el');
-var bigCardEl = document.querySelector('.big-card')
+var bigCardEl = document.querySelector('#big-card');
+
 
 var handleFormSubmit = function(event) {
     event.preventDefault();
@@ -11,11 +12,27 @@ var handleFormSubmit = function(event) {
     bigCardEl.textContent = '';
     if (city) {
          getCityCoordenates(city);
+         getCurrentWeather(city);
         console.log(city);
     } else {
         alert('Please enter a city');
     }
 }
+
+var getCurrentWeather = function(city) {
+    let currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid='+ apiKey + '&units=imperial';
+    fetch(currentWeatherUrl)
+        .then(function(response) {
+            if(response.ok) {
+                response.json()
+                .then(function(data) {
+                    displayCurrentWeather(data);
+                    console.log(data);
+                })
+            }
+        })
+}
+
 
 var getCityCoordenates = function(cityParameter) {
     var queryUrl =  'https://api.openweathermap.org/data/2.5/forecast?q=' + cityParameter + '&appid=' + apiKey + '&units=imperial';
@@ -25,6 +42,7 @@ var getCityCoordenates = function(cityParameter) {
                 response.json()
                 .then(function(data) {
                     displayWeatherCards(data);
+                    console.log(data);
                 })
             } else { 
                 alert('Error' + response.statusText);
@@ -34,34 +52,83 @@ var getCityCoordenates = function(cityParameter) {
         })
 }
 
+var displayCurrentWeather = function(currentWeather) {
+    
+    var today = dayjs().format('DD-MMM-YY');
+    var titleEl = document.createElement('h1');
+    
+
+    var tempEl = document.createElement('p');
+    var windEl = document.createElement('p');
+    var humidityEl = document.createElement('p');
+
+    tempEl.textContent = 'Temp: ' + currentWeather.main.temp + ' °F';
+    windEl.textContent = 'Wind: ' + currentWeather.wind.speed + ' MPH';
+    humidityEl.textContent = 'Humidity: ' + currentWeather.main.humidity + ' %';
+    titleEl.textContent = inputEl.value + ' ' + '(' + today + ')';
+
+   if(currentWeather.weather[0].main === "Clouds") {
+       
+        let cloudsIcon = document.createElement('í');
+        cloudsIcon.style.color = '#302ae5';
+        cloudsIcon.classList.add('fa-cloud-sun', 'fa-xl', 'fa-solid');
+        titleEl.append(cloudsIcon);
+        
+   }  else if(currentWeather.weather[0].main === "Clear") {
+        
+        let clearIcon = document.createElement('í');
+        clearIcon.style.color = '#c6de17';
+        clearIcon.classList.add('fa-solid', 'fa-sun', 'fa-xl');
+        titleEl.append(clearIcon);
+
+   } else if(currentWeather.weather[0].main === "Drizzle") {
+
+        let drizzleIcon = document.createElement('í');
+        drizzleIcon.style.color = '#255ec1';
+        drizzleIcon.classList.add('fa-solid', 'fa-cloud-rain');
+        titleEl.append(drizzleIcon);
+
+   } else if (currentWeather.weather[0].main === "Mist") {
+        
+        let mistIcon = document.createElement('í');
+        mistIcon.style.color = '#315d68';
+        mistIcon.classList.add('fa-solid', 'fa-cloud');
+        titleEl.append(mistIcon);
+
+   }
+   
+    bigCardEl.append(titleEl, tempEl, windEl, humidityEl);
+    bigCardEl.classList = 'big-card';
+}
+
 var displayWeatherCards = function(weather) {
     if(weather === 0) {
         alert("There's no data for this city, sorry");
         return;
     } 
-    var today = dayjs();
-    var dateEl = document.createElement('h2');
-    var titleEl = document.createElement('h1');
-    dateEl.textContent = today.format('DD-MMM-YY');
-    titleEl.classList = 'date';
-    dateEl.classList = 'appendedtitle';
-    titleEl.textContent = inputEl.value;
-    titleEl.appendChild(dateEl);
-    bigCardEl.appendChild(titleEl);
-    var todayTemp = weather.list[0].main.temp;
-    console.log(todayTemp);
-    var todayWind = weather.list[0].wind.speed;
-    var todayTempEl = document.createElement('p');
-    todayTempEl.textContent = 'Todays temp: ' + todayTemp + ' °F';
-    titleEl.append(todayTempEl);
-    
-    
+    // var today = dayjs();
+    // var titleEl = document.createElement('h1');
+    // titleEl.textContent = inputEl.value + ' ' + '(' +today.format('DD-MMM-YY') + ')';
+    // bigCardEl.appendChild(titleEl);
+    // bigCardEl.classList = 'big-card';
+    // var todayTemp = weather.list[2].main.temp;
+    // var todayWind = weather.list[2].wind.speed;
+    // var todayHumidity = weather.list[2].main.humidity;
+    // var todayTempEl = document.createElement('p');
+    // var todayWindEl = document.createElement('p');
+    // var todayHumidityEl = document.createElement('p');
+    // todayWindEl.textContent = 'Wind speed: ' + todayWind + ' MPH';
+    // todayTempEl.textContent = 'Temp: ' + todayTemp + ' °F';
+    // todayHumidityEl.textContent = 'Humidity: ' + todayHumidity + ' %';
+    // bigCardEl.append(todayTempEl);
+    // bigCardEl.append(todayWindEl);
+    // bigCardEl.append(todayHumidityEl);
     
 
     
     console.log(weather.list[0]);
-    for(var i = 0; i < weather.length; i+=8) {
-        console.log(data[i]);
+    for(var i = 0; i < weather.list.length; i+=8) {
+        console.log(weather.list[i]);
         
     }
 
